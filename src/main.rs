00e68538c333c6af7779f81605b4ab2281122411
Noam Yorav-Raphael <noamraph@gmail.com>
2024-10-25 13:57:20 +0300

    Support strings

--- src/main.rs
+++ src/main.rs
@@ -27,35 +27,53 @@ enum TokenType {
     Less,
     LessEqual,
 
+    // Literals
+    Identifier(String),
+    StringLiteral(String),
+    Number(String),
+
     Eof,
 }
 
 use TokenType::*;
 
-fn token_type_name(token: TokenType) -> &'static str {
-    match token {
-        LeftParen => "LEFT_PAREN",
-        RightParen => "RIGHT_PAREN",
-        LeftBrace => "LEFT_BRACE",
-        RightBrace => "RIGHT_BRACE",
-        Comma => "COMMA",
-        Dot => "DOT",
-        Minus => "MINUS",
-        Plus => "PLUS",
-        Semicolon => "SEMICOLON",
-        Slash => "SLASH",
-        Star => "STAR",
+impl TokenType {
+    fn name(&self) -> &'static str {
+        match self {
+            LeftParen => "LEFT_PAREN",
+            RightParen => "RIGHT_PAREN",
+            LeftBrace => "LEFT_BRACE",
+            RightBrace => "RIGHT_BRACE",
+            Comma => "COMMA",
+            Dot => "DOT",
+            Minus => "MINUS",
+            Plus => "PLUS",
+            Semicolon => "SEMICOLON",
+            Slash => "SLASH",
+            Star => "STAR",
 
-        Bang => "BANG",
-        BangEqual => "BANG_EQUAL",
-        Equal => "EQUAL",
-        EqualEqual => "EQUAL_EQUAL",
-        Greater => "GREATER",
-        GreaterEqual => "GREATER_EQUAL",
-        Less => "LESS",
-        LessEqual => "LESS_EQUAL",
+            Bang => "BANG",
+            BangEqual => "BANG_EQUAL",
+            Equal => "EQUAL",
+            EqualEqual => "EQUAL_EQUAL",
+            Greater => "GREATER",
+            GreaterEqual => "GREATER_EQUAL",
+            Less => "LESS",
+            LessEqual => "LESS_EQUAL",
 
-        Eof => "EOF",
+            Identifier(_) => "IDENTIFIER",
+            StringLiteral(_) => "STRING",
+            Number(_) => "NUMBER",
+
+            Eof => "EOF",
+        }
+    }
+
+    fn literal_str(&self) -> String {
+        match self {
+            StringLiteral(s) => s.clone(),
+            _ => "null".into(),
+        }
     }
 }
 
@@ -73,80 +91,99 @@ fn is_match<I: Iterator<Item = char>>(iter: &mut Peekable<I>, c: char) -> bool {
     }
 }
 
+fn error(line: usize, had_error: &mut bool, msg: &str) {
+    eprintln!("[line {}] Error: {}", line, msg);
+    *had_error = true;
+}
+
 fn scan_token<I: Iterator<Item = char>>(
     iter: &mut Peekable<I>,
     line: &mut usize,
     had_error: &mut bool,
 ) -> Option<Token> {
-    loop {
-        let c = iter.next()?;
-        let mut lexeme = c.to_string();
-        let maybe_token = match c {
-            ' ' | '\t' => None,
-            '\n' => {
-                *line += 1;
-                None
-            }
-            '(' => Some(LeftParen),
-            ')' => Some(RightParen),
-            '{' => Some(LeftBrace),
-            '}' => Some(RightBrace),
-            ',' => Some(Comma),
-            '.' => Some(Dot),
-            '-' => Some(Minus),
-            '+' => Some(Plus),
-            ';' => Some(Semicolon),
-            '*' => Some(Star),
-
-            '!' | '=' | '>' | '<' => {
-                if is_match(iter, '=') {
-                    lexeme.push('=');
-                    match c {
-                        '!' => Some(BangEqual),
-                        '=' => Some(EqualEqual),
-                        '>' => Some(GreaterEqual),
-                        '<' => Some(LessEqual),
-                        _ => unreachable!(),
-                    }
-                } else {
-                    match c {
-                        '!' => Some(Bang),
-                        '=' => Some(Equal),
-                        '>' => Some(Greater),
-                        '<' => Some(Less),
-                        _ => unreachable!(),
-                    }
-                }
-            }
-
-            '/' => {
-                if is_match(iter, '/') {
-                    loop {
-                        match iter.next() {
-                            Some('\n') => {
-                                *line += 1;
-                                break;
-                            }
-                            None => break,
-                            _ => (),
-                        }
-                    }
-                    None
-                } else {
-                    Some(Slash)
-                }
-            }
-
-            _ => {
-                eprintln!("[line {}] Error: Unexpected character: {}", line, c);
-                *had_error = true;
-                None
-            }
-        };
-        if let Some(token_type) = maybe_token {
-            return Some(Token { token_type, lexeme });
+    let c = iter.next()?;
+    let mut lexeme = c.to_string();
+    let token_type = match c {
+        ' ' | '\t' => return None,
+        '\n' => {
+            *line += 1;
+            return None;
         }
-    }
+        '(' => LeftParen,
+        ')' => RightParen,
+        '{' => LeftBrace,
+        '}' => RightBrace,
+        ',' => Comma,
+        '.' => Dot,
+        '-' => Minus,
+        '+' => Plus,
+        ';' => Semicolon,
+        '*' => Star,
+
+        '!' | '=' | '>' | '<' => {
+            if is_match(iter, '=') {
+                lexeme.push('=');
+                match c {
+                    '!' => BangEqual,
+                    '=' => EqualEqual,
+                    '>' => GreaterEqual,
+                    '<' => LessEqual,
+                    _ => unreachable!(),
+                }
+            } else {
+                match c {
+                    '!' => Bang,
+                    '=' => Equal,
+                    '>' => Greater,
+                    '<' => Less,
+                    _ => unreachable!(),
+                }
+            }
+        }
+
+        '/' => {
+            if is_match(iter, '/') {
+                loop {
+                    match iter.next() {
+                        Some('\n') => {
+                            *line += 1;
+                            break;
+                        }
+                        None => break,
+                        _ => (),
+                    }
+                }
+                return None;
+            } else {
+                Slash
+            }
+        }
+
+        '"' => {
+            let mut value = String::new();
+            loop {
+                let Some(c) = iter.next() else {
+                    error(*line, had_error, "Unterminated string.");
+                    return None;
+                };
+                lexeme.push(c);
+                if c == '\n' {
+                    *line += 1;
+                }
+                if c == '"' {
+                    break;
+                }
+                value.push(c);
+            }
+            StringLiteral(value)
+        }
+
+        _ => {
+            error(*line, had_error, &format!("Unexpected character: {}", c));
+            return None;
+        }
+    };
+    Some(Token { token_type, lexeme })
 }
 
 fn tokenize(contents: &str) -> (Vec<Token>, bool) {
@@ -154,8 +191,10 @@ fn tokenize(contents: &str) -> (Vec<Token>, bool) {
     let mut had_error = false;
     let mut tokens = Vec::<Token>::new();
     let mut iter = contents.chars().peekable();
-    while let Some(token) = scan_token(&mut iter, &mut line, &mut had_error) {
-        tokens.push(token);
+    while iter.peek().is_some() {
+        if let Some(token) = scan_token(&mut iter, &mut line, &mut had_error) {
+            tokens.push(token);
+        }
     }
     tokens.push(Token {
         token_type: Eof,
@@ -169,9 +208,10 @@ fn cmd_tokenize(filename: &str) -> ExitCode {
     let (tokens, had_error) = tokenize(&file_contents);
     for token in tokens {
         println!(
-            "{} {} null",
-            token_type_name(token.token_type),
-            token.lexeme
+            "{} {} {}",
+            token.token_type.name(),
+            token.lexeme,
+            token.token_type.literal_str()
         );
     }
     if had_error {
